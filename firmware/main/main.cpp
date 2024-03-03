22f326e91c3afde4ac7e94fb626f6948b726e954
c-porto <carlos.portof@hotmail.com>
2024-03-03 20:24:32 -0300

    firmware: Add NVS to use wifi.

--- firmware/main/main.cpp
+++ firmware/main/main.cpp
@@ -6,11 +6,11 @@
 #include "freertos/task.h"
 #include "task_api.hpp"
 #include "utils.hpp"
+#include "nvs_flash.h"
 
 /* Defining the app_main function*/
-extern "C"
-{
-    void app_main();
+extern "C" {
+void app_main();
 }
 
 namespace
@@ -23,13 +23,21 @@ sensor::Measure ms;
 /* Main function */
 extern "C" void app_main(void)
 {
-    /* Changing Main task priority to initialize all other tasks first */
-    vTaskPrioritySet(NULL, 10);
-    /* Creates mutexes*/
-    create_mutex();
-    /* Creates event groups*/
-    create_event_groups();
-    /* Creates tasks */
-    create_tasks(static_cast<void *>(&ms));
-    /* Espressif-idf Framework starts the FreeRTOS scheduler automatically*/
+	/* Changing Main task priority to initialize all other tasks first */
+	vTaskPrioritySet(NULL, 10);
+	// Initialize NVS
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+	    ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+	/* Creates mutexes*/
+	create_mutex();
+	/* Creates event groups*/
+	create_event_groups();
+	/* Creates tasks */
+	create_tasks(static_cast<void *>(&ms));
+	/* Espressif-idf Framework starts the FreeRTOS scheduler automatically*/
 }
