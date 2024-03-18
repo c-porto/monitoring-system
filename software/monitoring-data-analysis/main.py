400a145d2389befdae5ba9890c75810fada5c123
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2024-03-17 21:10:18 -0300

    software: monitoring-data-analysis: Adding visual graph representations of sensor data #3

--- software/monitoring-data-analysis/main.py
+++ software/monitoring-data-analysis/main.py
@@ -3,26 +3,23 @@ import time
 import threading as th
 
 from stats import Metrics, get_data_statistics, prepare_samples_for_plotting
+from visuals import plot_visuals
 
 ALL_SAMPLES_ENDPOINT: str = 'http://0.0.0.0:42068/api/get_samples'
 LAST_100_SAMPLES_ENDPOINT: str = 'http://0.0.0.0:42068/api/ui_update'
+STATS_DISPLAY_PERIOD_SEC = 60
 
 def stats_display():
     while True:
         get_data_statistics(LAST_100_SAMPLES_ENDPOINT)
-        time.sleep(300)
+        time.sleep(STATS_DISPLAY_PERIOD_SEC)
 
-def graph_display(samples: dict[Metrics, list]):
-    # TODO
-    while True:
-        print("Teste2")
-        time.sleep(2)
+def visuals_display(samples: dict[Metrics, list] | None, display_flags: list[bool]):
+    plot_visuals(display_flags, samples, None)
 
-def dist_display(samples: dict[Metrics, list]):
-    # TODO
     while True:
-        print("Teste1")
-        time.sleep(5)
+        # The plot_visuals function handles refreshing by itself 
+        plot_visuals(display_flags, None, ALL_SAMPLES_ENDPOINT)
 
 def setup_jobs(display_flags: list[bool]):
     samples_dict: dict[Metrics, list] | None = None
@@ -34,13 +31,7 @@ def setup_jobs(display_flags: list[bool]):
     if any(display_flags):
         samples_dict = prepare_samples_for_plotting(ALL_SAMPLES_ENDPOINT)
 
-    if display_flags[0]:
-        graph_thread = th.Thread(target=graph_display, args=(samples_dict,))
-        graph_thread.start()
-
-    if display_flags[1]:
-        dist_thread = th.Thread(target=dist_display, args=(samples_dict,))
-        dist_thread.start()
+        visuals_display(samples_dict, display_flags)
 
 def monitoring_system_cli():
     cli_parser = argparse.ArgumentParser(description="Monitoring System CLI data visualization")
